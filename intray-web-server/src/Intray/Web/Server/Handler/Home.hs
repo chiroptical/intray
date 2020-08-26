b3f5fc6956fa0235e7b89b3a94f429065544474c
Tom Sydney Kerckhove <syd@cs-syd.eu>
2020-08-26 04:38:06 +0200

    Upgraded lts

--- intray-web-server/src/Intray/Web/Server/Handler/Home.hs
+++ intray-web-server/src/Intray/Web/Server/Handler/Home.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE TemplateHaskell #-}
 
 module Intray.Web.Server.Handler.Home
@@ -5,14 +6,15 @@ module Intray.Web.Server.Handler.Home
   ) where
 
 import Import
-
-import Yesod
-
 import Intray.Client
 import Intray.Web.Server.Foundation
 import Intray.Web.Server.Handler.Pricing
+import Yesod
 
 getHomeR :: Handler Html
 getHomeR = do
   mPricing <- runClientOrErr clientGetPricing
-  withNavBar $(widgetFile "home")
+  withNavBar $ do
+    setTitle "Intray"
+    setDescription "A GTD In-box system"
+    $(widgetFile "home")
